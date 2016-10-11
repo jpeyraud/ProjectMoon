35b12e75ad926e26fe0d8c9a72c21bc25bd76071
JeremyX Peyraud <jeremyx.peyraud@intel.com>
2016-10-11 14:55:34 +0200

    start creating gameengine scripts

---
+++ Assets/ProjectMoon/Scripts/RessourcesManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RessourcesManager : MonoBehaviour {
+
+	public float wood;
+	public float stone;
+	public float iron;
+
+	public const float woodPerSecGeneration = 0.2f;
+	public const float stonePerSecGeneration = 0.2f;
+	public const float ironPerSecGeneration = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		wood = 0f;
+		wood = 0f;
+		wood = 0f;
+
+		InvokeRepeating("LaunchProjectile", 2.0f, 0.3f);
+	}
+
+
+}
